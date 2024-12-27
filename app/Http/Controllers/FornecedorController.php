4cd4fd48ed86c40e55eac999e870d2203a8ac1d2
andrepfdev <dreletras@gmail.com>
2024-12-26 22:34:39 -0300

    Fornecedores

---
+++ app/Http/Controllers/FornecedorController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Fornecedor;
+
+class FornecedorController extends Controller
+{
+    public function index()
+    {
+        return view('fornecedores.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\FornecedorController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 
 Route::view('/', 'produtos.index');
 
 Route::get('/produtos', [ProdutoController::class, 'index'])->name('produtos.index');
+
+
+Route::get('/fornecedores', [FornecedorController::class, 'index'])->name('fornecedores.index');
 
 
 
